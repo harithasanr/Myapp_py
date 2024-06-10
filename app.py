0e106665e2d458f8728a291dd51c63e219e3ee6f
harithasanr <133355853+harithasanr@users.noreply.github.com>
2024-06-10 17:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "AWS Code Pipeline: 'Hello_python'"
+    return "AWS Code Pipeline: 'Hello_python_1'"
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
